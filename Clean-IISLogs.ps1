b9c9516a254421305556954339ba600e6f19db65
ForensicITGuy <ForensicITGuy@users.noreply.github.com>
2016-05-06 11:07:09 -0500

    Added files via upload

---
+++ Clean-IISLogs.ps1
@@ -0,0 +1,12 @@
+﻿## Script Name:    Clean-IISLogs
+## Input/Output:   Directory of IIS Logs
+## Description:    Detects service state for predefined service in global variable.
+##                 If service is stopped, perform loop to start the service and log the start.
+##                 If service is started, log that fact.
+
+param ([Parameter(Mandatory=$True)][string]$LogPath)
+
+$retentionDays = 90
+$expirationDate = (Get-Date).AddDays(-$retentionDays)
+$expiredLogs = Get-ChildItem -Path $LogPath -Recurse | Where-Object {$_.LastWriteTime -lt $expirationDate}
+$expiredLogs | Remove-Item -Force -Recurse
